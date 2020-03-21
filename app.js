d6ec1a51e5731463787a2ff0321ff704869299bc
moaalaa <moaalaa16@gmail.com>
2020-03-21 03:20:21 +0200

    Added: Adding dotenv File

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-console.log("Application Running");
-
+// Modules
+// -- Init dotenv
+require('dotenv').config();
 const express = require('express');
 
+// "App" Web Server Init
 const app = express();
+
+// PORT Init
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('<h1>Welcome To Todo App</h1>');
 });
 
-app.listen(3000);
+app.listen(PORT, () => {
+    console.log(`Server Run On PORT ${PORT}`);
+});
 
 // localhost:3000
 // 127.0.0.1:3000
