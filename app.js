a04ff8d1dada384534a08c9f04cb753e0b343312
moaalaa <moaalaa16@gmail.com>
2020-03-21 02:20:30 +0200

    Added: Adding Basic Home Route

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-console.log("Application Started");
+console.log("Application Running");
 
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1>Welcome To Todo App</h1>');
+});
+
+app.listen(3000);
+
+// localhost:3000
+// 127.0.0.1:3000
