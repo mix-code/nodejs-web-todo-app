56fdd6347ee29f97af73ef455e8f64acef42f478
moaalaa <moaalaa16@gmail.com>
2020-03-24 04:41:28 +0200

    Added: Adding STatic Files

--- app.js
+++ app.js
@@ -2,15 +2,23 @@
 // -- Init dotenv
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 
 // "App" Web Server Init
 const app = express();
+
+// Init Static Files
+app.use('/assets', express.static(path.join(__dirname , 'public')));
+
+// Init Views
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
 // PORT Init
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('<h1>Welcome To Todo App</h1>');
+    res.render('test', {message: 'To My App'});
 });
 
 app.listen(PORT, () => {
